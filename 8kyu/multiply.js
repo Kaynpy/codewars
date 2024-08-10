3981e67bb9c0e129a11489a276f1e9126bcecd19
Anatoliy <kaynpy@gmail.com>
2024-08-10 18:38:27 +0300

    multiply

---
+++ 8kyu/multiply.js
@@ -0,0 +1,10 @@
+// ❓Description:
+//     This code does not execute properly. Try to figure out why.
+
+
+//✔️ SOLUTION:
+function multiply(a, b){
+    return a * b
+}
+
+console.log("multiply :", multiply(2,4));
